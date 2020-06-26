dd9127f89c682a55ced7d109fa817fed95659dba
wzy <842089160@qq.com>
2020-06-26 10:03:13 +0800

    开发自定义序列化器

--- imooc-coupon-service/coupon-template/src/main/java/com/imooc/coupon/entity/CouponTemplate.java
+++ imooc-coupon-service/coupon-template/src/main/java/com/imooc/coupon/entity/CouponTemplate.java
@@ -1,11 +1,13 @@
 package com.imooc.coupon.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.imooc.coupon.constant.CouponCategory;
 import com.imooc.coupon.constant.DistributeTarget;
 import com.imooc.coupon.constant.ProductLine;
 import com.imooc.coupon.converter.CouponCategoryConverter;
 import com.imooc.coupon.converter.ProductLineConverter;
 import com.imooc.coupon.converter.RuleConverter;
+import com.imooc.coupon.serialization.CouponTemplateSerialize;
 import com.imooc.coupon.vo.TemplateRule;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -32,6 +34,7 @@ import java.util.Date;
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "coupon-template")
+@JsonSerialize(using = CouponTemplateSerialize.class)
 public class CouponTemplate implements Serializable {
     /**
      * 自增主键
@@ -136,12 +139,10 @@ public class CouponTemplate implements Serializable {
         this.count = count;
         this.userId = userId;
 
-        // 优惠券唯一编码 = 4位产品线和类型 + 8位日期（20190101）+ id(扩充为四位)
+        // 优惠券模板唯一编码 = 4位产品线和类型 + 8位日期（20190101）+ id(扩充为四位)
         this.key = productLine.toString() + category +
                 new SimpleDateFormat("yyyyMMdd").format(new Date());
         this.target = DistributeTarget.of(target);
         this.rule = rule;
-
     }
-
 }
---
+++ imooc-coupon-service/coupon-template/src/main/java/com/imooc/coupon/serialization/CouponTemplateSerialize.java
@@ -0,0 +1,58 @@
+package com.imooc.coupon.serialization;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.imooc.coupon.entity.CouponTemplate;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+/**
+ * 描述：优惠券模板实体类自定义序列化器
+ * 为什么要有序列化器呢？因为比如ProductLine这种枚举类，直接序列化为Json，
+ * 并不会使用对应的描述而是使用整个枚举对象，因为我们序列化返回的值是给用户看的。所以我们需要对默认的序列化规则
+ * 进行调整。
+ *
+ * @Author wzy
+ * @Date 2020/6/26 9:45
+ * @Version V1.0
+ **/
+public class CouponTemplateSerialize extends JsonSerializer<CouponTemplate> {
+
+    /**
+     * 序列化方法
+     * @param template
+     * @param generator json生成器
+     * @param serializerProvider
+     * @throws IOException
+     */
+    @Override
+    public void serialize(CouponTemplate template,
+                          JsonGenerator generator,
+                          SerializerProvider serializerProvider) throws IOException {
+        // 开始序列化对象
+        generator.writeStartObject();
+
+        generator.writeStringField("id", template.getId().toString() );
+        generator.writeStringField("name", template.getName() );
+        generator.writeStringField("logo", template.getLogo() );
+        generator.writeStringField("desc", template.getDescription());
+        generator.writeStringField("category",
+                template.getCategory().getDescription());
+        generator.writeStringField("productLine",
+                template.getProductLine().getDescription());
+        generator.writeStringField("count", template.getCount().toString() );
+        generator.writeStringField("createTime",
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(template.getCreateTime()) );
+        generator.writeStringField("userId", template.getUserId().toString() );
+        generator.writeStringField("key", template.getKey() + String.format("%04d", template.getId()));
+        generator.writeStringField("target", template.getTarget().getDescription());
+        generator.writeStringField("rule",
+                JSON.toJSONString(template.getRule()));
+
+        // 结束序列化对象
+        generator.writeEndObject();
+    }
+}
