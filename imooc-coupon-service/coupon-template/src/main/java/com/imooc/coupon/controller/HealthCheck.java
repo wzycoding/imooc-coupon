25c6eb5a27a209d459654e1e0b5313b368379dc7
wzy <842089160@qq.com>
2020-06-27 23:50:32 +0800

    添加定时任务清除过期优惠券、添加健康检查接口

---
+++ imooc-coupon-service/coupon-template/src/main/java/com/imooc/coupon/controller/HealthCheck.java
@@ -0,0 +1,86 @@
+package com.imooc.coupon.controller;
+
+import com.imooc.coupon.exception.CouponException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 描述：健康检查接口
+ *
+ * @Author wzy
+ * @Date 2020/6/27 22:34
+ * @Version V1.0
+ **/
+@Slf4j
+@RestController
+public class HealthCheck {
+
+    /** 服务发现客户端 :获取其他服务的元信息**/
+    private final DiscoveryClient discoveryClient;
+
+    /**
+     * 服务注册接口，提供了获取服务id的方法：利用这个接口进行服务注册，
+     * 通过id再到discoveryClient获取元信息
+     */
+    private final Registration registration;
+
+    @Autowired
+    public HealthCheck(DiscoveryClient discoveryClient, Registration registration) {
+        this.discoveryClient = discoveryClient;
+        this.registration = registration;
+    }
+
+    /**
+     * 健康检查接口
+     * http://localhost:7001/coupon-template/health
+     */
+    @GetMapping("/health")
+    public String health() {
+        log.debug("view health api");
+        return "CouponTemplate Is OK!";
+    }
+
+    /**
+     * 异常测试接口
+     * http://localhost:7001/coupon-template/exception
+     */
+    @GetMapping("/exception")
+    public String exception() throws CouponException {
+        log.debug("view exception api");
+        throw new CouponException("CouponTemplate Has Some Problem");
+    }
+
+    /**
+     * 获取EurekaServer微服务元信息
+     */
+    @GetMapping("/info")
+    public List<Map<String, Object>> info() {
+        // 大约需要等待两分钟的时间才能获取到注册信息
+        // registration.getServiceId()当前服务的id
+        List<ServiceInstance> instances =
+                discoveryClient.getInstances(registration.getServiceId());
+        List<Map<String, Object>> result =
+                new ArrayList<>(instances.size());
+        instances.forEach( i -> {
+            Map<String, Object> info = new HashMap<>();
+            // 服务id
+            info.put("serviceId", i.getServiceId());
+            // 实例id，实例可以有多个，服务只能有一个
+            info.put("instanceId", i.getInstanceId());
+            info.put("port", i.getPort());
+
+            result.add(info);
+        });
+        return result;
+    }
+}
---
+++ imooc-coupon-service/coupon-template/src/main/java/com/imooc/coupon/schedule/ScheduledTask.java
@@ -0,0 +1,68 @@
+package com.imooc.coupon.schedule;
+
+import com.imooc.coupon.dao.CouponTemplateDao;
+import com.imooc.coupon.entity.CouponTemplate;
+import com.imooc.coupon.vo.TemplateRule;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 描述：清理过期优惠券模板定时任务
+ *
+ * @Author wzy
+ * @Date 2020/6/27 22:10
+ * @Version V1.0
+ **/
+@Slf4j
+@Component
+public class ScheduledTask {
+    /**
+     * CouponTemplateDao
+     */
+    private final CouponTemplateDao templateDao;
+
+    @Autowired
+    public ScheduledTask(CouponTemplateDao templateDao) {
+        this.templateDao = templateDao;
+    }
+
+    /**
+     * 下线过期优惠券模板，每隔60分钟执行一次
+     */
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void offlineCouponTemplate() {
+        log.info("Start To Expire CouponTemplate");
+        List<CouponTemplate> templates =
+                templateDao.findAllByExpired(false);
+        if (CollectionUtils.isEmpty(templates)) {
+            log.info("Done To Expire CouponTemplate.");
+            return;
+        }
+
+        Date cur = new Date();
+        List<CouponTemplate> expiredTemplates =
+                new ArrayList<>(templates.size());
+        templates.forEach( t -> {
+            // 根据优惠券模板规则中的“过期规则”进行判断
+            TemplateRule rule = t.getRule();
+            // 比当前时间小，说明已经过期
+            if (rule.getExpiration().getDeadLine() < cur.getTime()) {
+                t.setExpired(true);
+                expiredTemplates.add(t);
+            }
+
+        });
+        if (CollectionUtils.isNotEmpty(expiredTemplates)) {
+            log.info("Expired CouponTemplate Num: {}",
+                    templateDao.saveAll(expiredTemplates));
+        }
+        log.info("Done To Expire CouponTemplate.");
+    }
+}
