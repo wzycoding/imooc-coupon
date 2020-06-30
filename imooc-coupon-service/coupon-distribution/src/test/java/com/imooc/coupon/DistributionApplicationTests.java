009fb0c297b8b6e30254f41a376d9156f07941a8
wzy <842089160@qq.com>
2020-06-30 23:46:33 +0800

    添加sql文件、单元测试环境校验

---
+++ imooc-coupon-service/coupon-distribution/src/test/java/com/imooc/coupon/DistributionApplicationTests.java
@@ -0,0 +1,23 @@
+package com.imooc.coupon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 描述：分发微服务测试用例环境校验
+ *
+ * @Author wzy
+ * @Date 2020/6/30 21:34
+ * @Version V1.0
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DistributionApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
